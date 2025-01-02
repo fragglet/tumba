a5ad7b02335365ee10270fdea90bbcf5a243724b
Simon Howard <fraggle@soulsphere.org>
2025-01-02 17:25:22 -0500

    Initial scripts for running Samba torture tests

---
+++ torture/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# TODO: This should eventually be a Github Action CI test
+
+mkdir -p z
+ln -s $(which python2) z/python
+export PATH="$PWD/z:$PATH" CFLAGS="-fpermissive"
+cd samba
+sed -i "s/HAVE_BSD_MD5_H/disabled_BSD_MD5_H/" lib/crypto/md5.h
+./configure --disable-python \
+            --without-ad-dc \
+            --without-acl-support \
+            --without-systemd \
+            --without-gettext \
+            --without-ads \
+            --without-pam \
+            --without-ntvfs-fileserver  \
+            --without-quotas \
+            --without-dmapi  \
+            --without-libarchive  \
+            --without-sendfile-support \
+            --without-gpgme \
+            --without-syslog \
+            --without-ldap
+make smbtorture3
