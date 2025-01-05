4e06c89117a7fa1dd134e7b2abb792cd6db518c9
Simon Howard <fraggle@soulsphere.org>
2025-01-04 20:22:20 -0500

    Delete now-unused constants

--- src/local.h
+++ src/local.h
@@ -5,14 +5,6 @@
 #ifndef _LOCAL_H
 #define _LOCAL_H
 
-/* This defines the section name in the configuration file that will
-   refer to the special "printers" service */
-#define PRINTERS_NAME "printers"
-
-/* Yves Gaige <yvesg@hptnodur.grenoble.hp.com> requested this */
-/* Set to 8 if old clients break because of this, the max value is 15 */
-#define MAXPRINTERLEN 15
-
 /* define what facility to use for syslog */
 #ifndef SYSLOG_FACILITY
 #define SYSLOG_FACILITY LOG_DAEMON
@@ -25,24 +17,10 @@
 #define MAX_CONNECTIONS 127
 #define MAX_OPEN_FILES  100
 
-/* the max number of connections that the smbstatus program will show */
-#define MAXSTATUS 1000
-
 /* max number of directories open at once */
 /* note that with the new directory code this no longer requires a
    file handle per directory, but large numbers do use more memory */
 #define MAXDIR 64
-
-#define WORDMAX 0xFFFF
-
-/* the maximum password length before we declare a likely attack */
-#define MAX_PASS_LEN 200
-
-/* separators for lists */
-#define LIST_SEP " \t,;:\n\r"
-
-/* this is where browse lists are kept in the lock dir */
-#define SERVER_LIST "browse.dat"
 
 /* shall filenames with illegal chars in them get mangled in long
    filename listings? */
@@ -65,62 +43,17 @@
 #define PRIME_NMBD 1
 #endif
 
-/* the default pager to use for the client "more" command. Users can
-   override this with the PAGER environment variable */
-#ifndef PAGER
-#define PAGER "more"
-#endif
-
-/* the size of the uid cache used to reduce valid user checks */
-#define UID_CACHE_SIZE 4
-
 /* the following control timings of various actions. Don't change
    them unless you know what you are doing. These are all in seconds */
 #define DEFAULT_SMBD_TIMEOUT  (60 * 60 * 24 * 7)
-#define SMBD_RELOAD_CHECK     (60)
 #define IDLE_CLOSED_TIMEOUT   (60)
 #define DPTR_IDLE_TIMEOUT     (120)
 #define SMBD_SELECT_LOOP      (10)
-#define NMBD_SELECT_LOOP      (10)
-#define BROWSE_INTERVAL       (60)
 #define REGISTRATION_INTERVAL (10 * 60)
-#define NMBD_INETD_TIMEOUT    (120)
-#define NMBD_MAX_TTL          (24 * 60 * 60)
 
 /* the following are in milliseconds */
 #define LOCK_RETRY_TIMEOUT (100)
 
-/* do you want to dump core (carefully!) when an internal error is
-   encountered? Samba will be careful to make the core file only
-   accessible to root */
-#define DUMP_CORE 1
-
-/* what is the longest significant password available on your system?
- Knowing this speeds up password searches a lot */
-#ifndef PASSWORD_LENGTH
-#define PASSWORD_LENGTH 8
-#endif
-
 #define SMB_ALIGNMENT 1
-
-/* shall we support browse requests via a FIFO to nmbd? */
-#define ENABLE_FIFO 1
-
-/* how long to wait for a socket connect to happen */
-#define LONG_CONNECT_TIMEOUT  30
-#define SHORT_CONNECT_TIMEOUT 5
-
-/* the directory to sit in when idle */
-/* #define IDLE_DIR "/" */
-
-/* Timout (in seconds) to wait for an oplock break
-   message to return from the client. */
-
-#define OPLOCK_BREAK_TIMEOUT 30
-
-/* Timout (in seconds) to add to the oplock break timeout
-   to wait for the smbd to smbd message to return. */
-
-#define OPLOCK_BREAK_TIMEOUT_FUDGEFACTOR 2
 
 #endif
