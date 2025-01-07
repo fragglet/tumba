fragglet/tumba

commit fd9598379a10248aa74b6d6519f251c746961270

Delete `sys_rename()` wrapper, x-fs copies
The only purpose of this wrapper around the standard `rename` appears to
be to support copy across filesystems as a fallback. I think it's a
sufficiently obscure corner case that we can drop support for it,
especially as the code looks rather fragile.

src/reply.c
@@ -2318,8 +2318,9 @@ int reply_mv(char *inbuf, char *outbuf, int dum_size, int dum_buffsize)
 
 		if (resolve_wildcards(directory, newname) &&
 		    can_rename(directory, cnum) && !file_exist(newname, NULL) &&
-		    !sys_rename(directory, newname))
+		    rename(directory, newname) != 0) {
 			count++;
+		}
 
 		DEBUG(3, ("reply_mv : %s doing rename on %s -> %s\n",
 		          (count != 0) ? "succeeded" : "failed", directory,
@@ -2376,8 +2377,9 @@ int reply_mv(char *inbuf, char *outbuf, int dum_size, int dum_buffsize)
 					error = 183;
 					continue;
 				}
-				if (!sys_rename(fname, destname))
+				if (rename(fname, destname) != 0) {
 					count++;
+				}
 				DEBUG(3,
 				      ("reply_mv : doing rename on %s -> %s\n",
 				       fname, destname));

src/proto.h
@@ -190,7 +190,6 @@ int vslprintf(char *str, int n, char *format, va_list ap);
 int sys_select(fd_set *fds, struct timeval *tval);
 int sys_select(fd_set *fds, struct timeval *tval);
 int sys_utime(char *fname, struct utimbuf *times);
-int sys_rename(char *from, char *to);
 ssize_t sys_getxattr(const char *path, const char *name, void *value,
                      size_t size);
 ssize_t sys_setxattr(const char *path, const char *name, void *value,

src/system.c
@@ -76,113 +76,6 @@ int sys_utime(char *fname, struct utimbuf *times)
 	return utime(fname, times);
 }
 
-/*********************************************************
-for rename across filesystems Patch from Warren Birnbaum
-<warrenb@hpcvscdp.cv.hp.com>
-**********************************************************/
-
-static int copy_reg(char *source, const char *dest)
-{
-	struct stat source_stats;
-	int ifd;
-	int ofd;
-	char *buf;
-	int len; /* Number of bytes read into `buf'. */
-
-	lstat(source, &source_stats);
-	if (!S_ISREG(source_stats.st_mode)) {
-		return 1;
-	}
-
-	if (unlink(dest) && errno != ENOENT) {
-		return 1;
-	}
-
-	if ((ifd = open(source, O_RDONLY, 0)) < 0) {
-		return 1;
-	}
-	if ((ofd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0600)) < 0) {
-		close(ifd);
-		return 1;
-	}
-
-	if ((buf = malloc(COPYBUF_SIZE)) == NULL) {
-		close(ifd);
-		close(ofd);
-		unlink(dest);
-		return 1;
-	}
-
-	while ((len = read(ifd, buf, COPYBUF_SIZE)) > 0) {
-		if (write_data(ofd, buf, len) < 0) {
-			close(ifd);
-			close(ofd);
-			unlink(dest);
-			free(buf);
-			return 1;
-		}
-	}
-	free(buf);
-	if (len < 0) {
-		close(ifd);
-		close(ofd);
-		unlink(dest);
-		return 1;
-	}
-
-	if (close(ifd) < 0) {
-		close(ofd);
-		return 1;
-	}
-	if (close(ofd) < 0) {
-		return 1;
-	}
-
-	/* chown turns off set[ug]id bits for non-root,
-	   so do the chmod last.  */
-
-	/* Try to copy the old file's modtime and access time.  */
-	{
-		struct utimbuf tv;
-
-		tv.actime = source_stats.st_atime;
-		tv.modtime = source_stats.st_mtime;
-		if (utime(dest, &tv)) {
-			return 1;
-		}
-	}
-
-	/* Try to preserve ownership.  For non-root it might fail, but that's
-	   ok. But root probably wants to know, e.g. if NFS disallows it.  */
-	if (chown(dest, source_stats.st_uid, source_stats.st_gid) &&
-	    (errno != EPERM)) {
-		return 1;
-	}
-
-	if (chmod(dest, source_stats.st_mode & 07777)) {
-		return 1;
-	}
-	unlink(source);
-	return 0;
-}
-
-/*******************************************************************
-for rename()
-********************************************************************/
-int sys_rename(char *from, char *to)
-{
-	int rcode;
-	pstring zfrom, zto;
-
-	rcode = rename(from, to);
-
-	if (errno == EXDEV) {
-		/* Rename across filesystems needed. */
-		rcode = copy_reg(zfrom, zto);
-	}
-	return rcode;
-}
-
 /* Different OSes have different versions of getxattr */
 ssize_t sys_getxattr(const char *path, const char *name, void *value,
                      size_t size)

commit 77ed9521eca06496d823320a272b11314c02cacf

Fix backwards logic checking `rename()` result
Bug introduced during refactoring in commit fd9598379a10248aa74b.

src/reply.c
@@ -2414,7 +2414,7 @@ int reply_mv(char *inbuf, char *outbuf, int dum_size, int dum_buffsize)
 
 		if (resolve_wildcards(directory, newname) &&
 		    can_rename(directory, cnum) && !file_exist(newname, NULL) &&
-		    rename(directory, newname) != 0) {
+		    rename(directory, newname) == 0) {
 			count++;
 		}
 
@@ -2473,7 +2473,7 @@ int reply_mv(char *inbuf, char *outbuf, int dum_size, int dum_buffsize)
 					error = 183;
 					continue;
 				}
-				if (rename(fname, destname) != 0) {
+				if (rename(fname, destname) == 0) {
 					count++;
 				}
 				DEBUG(3,