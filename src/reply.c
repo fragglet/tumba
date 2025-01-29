27554a7d451806b32aa49db8d6398bb55a52df23
Simon Howard <fraggle@soulsphere.org>
2025-01-29 11:44:33 -0500

    Move includes into `.c` files

--- src/reply.c
+++ src/reply.c
@@ -23,6 +23,41 @@
    makes to handle specific protocols
 */
 
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pwd.h>
+#include <strings.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <utime.h>
+
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #include "includes.h"
 #include "trans2.h"
 
--- src/shares.c
+++ src/shares.c
@@ -17,8 +17,41 @@
 */
 
 #include <assert.h>
-#include <libgen.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <arpa/inet.h>
+#include <libgen.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pwd.h>
+#include <strings.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <utime.h>
+
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "includes.h"
 #include "smb.h"
