8e22f8b0a6d002d677569cc27c4d84fde97d2c21
Simon Howard <fraggle@soulsphere.org>
2025-01-29 11:26:56 -0500

    Rationalize includes list somewhat
    This separates the headers into sections by standard C headers/POSIX
    headers.

--- src/includes.h
+++ src/includes.h
@@ -27,41 +27,41 @@
 */
 
 #include "local.h"
+
 #include <ctype.h>
-#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdarg.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <time.h>
-#include <utime.h>
 
-#include <errno.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <fcntl.h>
 #include <grp.h>
 #include <net/if.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <pwd.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <stddef.h>
+#include <strings.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <utime.h>
+
 #include <sys/file.h>
 #include <sys/ioctl.h>
 #include <sys/param.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <syslog.h>
-#include <unistd.h>
-
-#include <arpa/inet.h>
-#include <dirent.h>
-#include <limits.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <strings.h>
 
 #define SIGNAL_CAST (void (*)(int))
 #define USE_SIGPROCMASK
