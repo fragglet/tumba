ee4aea89b821fb6d54ecfb7f4525ae04e8183b7f
Simon Howard <fraggle@soulsphere.org>
2025-01-30 11:54:16 -0500

    Delete `local.h`

--- src/local.h
+++
@@ -1,8 +0,0 @@
-/* Copyright (C) 1995-1998 Samba-Team */
-/* Copyright (C) 1998 John H Terpstra <jht@aquasoft.com.au> */
-
-/* local definitions for file server */
-#ifndef _LOCAL_H
-#define _LOCAL_H
-
-#endif
