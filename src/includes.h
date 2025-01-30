1285851e0585beebde13d92d5cc231ba05160b9f
Simon Howard <fraggle@soulsphere.org>
2025-01-30 12:12:57 -0500

    Move `strl{cpy,cat}` prototypes into `strfunc.h`

--- src/includes.h
+++ src/includes.h
@@ -46,8 +46,4 @@
 #define calloc(x, y)  __ERROR__XX_NEVER_USE_CALLOC__;
 #define strdup(x)     __ERROR__XX_NEVER_USE_STRDUP__;
 
-/* TODO: Remove these once their addition to glibc is less recent */
-size_t strlcat(char *, const char *, size_t);
-size_t strlcpy(char *, const char *, size_t);
-
 #endif
--- src/strfunc.h
+++ src/strfunc.h
@@ -17,6 +17,7 @@
 */
 
 #include <stdbool.h>
+#include <stddef.h>
 
 #define pstrcpy(d, s) safe_strcpy((d), (s), sizeof(pstring))
 #define pstrcat(d, s) safe_strcat((d), (s), sizeof(pstring))
@@ -50,3 +51,7 @@ int put_unicode(char *dst, char *src);
 char *safe_strcpy(char *dest, const char *src, int dest_size);
 char *safe_strcat(char *dest, const char *src, int dest_size);
 char *tab_depth(int depth);
+
+/* TODO: Remove these once their addition to glibc is less recent */
+size_t strlcat(char *, const char *, size_t);
+size_t strlcpy(char *, const char *, size_t);
