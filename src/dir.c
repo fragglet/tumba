fragglet/tumba

commit 570571de35a1af5aeed287e51bf031c61ec99111

Add prefix/suffix check functions for clarity
This also refactors some of the existing code to use the new functions.

src/dir.c
@@ -393,9 +393,7 @@ bool get_dir_entry(int cnum, char *mask, int dirtype, char *fname, int *size,
 	             strequal(Connections[cnum].dirpath, ".") ||
 	             strequal(Connections[cnum].dirpath, "/"));
 
-	needslash =
-	    (Connections[cnum].dirpath[strlen(Connections[cnum].dirpath) - 1] !=
-	     '/');
+	needslash = string_has_suffix(Connections[cnum].dirpath, "/");
 
 	if (!Connections[cnum].dirptr)
 		return false;

src/ipc.c
@@ -65,12 +65,6 @@ static int copy_and_advance(char **dst, char *src, int *n)
 	return l;
 }
 
-/* Check a API string for validity when we only need to check the prefix */
-static bool prefix_ok(char *str, char *prefix)
-{
-	return strncmp(str, prefix, strlen(prefix)) == 0;
-}
-
 static void send_trans_reply(char *outbuf, char *data, char *param,
                              uint16_t *setup, int ldata, int lparam, int lsetup)
 {
@@ -174,7 +168,7 @@ static bool api_RNetServerEnum(int cnum, char *param, char *data, int mdrcnt,
 	char *p = skip_string(str2);
 	int uLevel = SVAL(p, 0);
 
-	if (!prefix_ok(str1, "WrLehD"))
+	if (!string_has_prefix(str1, "WrLehD"))
 		return false;
 	if (!check_server_info(uLevel, str2))
 		return false;
@@ -336,7 +330,7 @@ static bool api_RNetShareGetInfo(int cnum, char *param, char *data, int mdrcnt,
 	}
 
 	/* check it's a supported varient */
-	if (!prefix_ok(str1, "zWrLh"))
+	if (!string_has_prefix(str1, "zWrLh"))
 		return false;
 	if (!check_share_info(uLevel, str2))
 		return false;
@@ -373,7 +367,7 @@ static bool api_RNetShareEnum(int cnum, char *param, char *data, int mdrcnt,
 	int data_len, fixed_len, string_len;
 	int f_len = 0, s_len = 0;
 
-	if (!prefix_ok(str1, "WrLeh"))
+	if (!string_has_prefix(str1, "WrLeh"))
 		return false;
 	if (!check_share_info(uLevel, str2))
 		return false;
@@ -478,7 +472,7 @@ static bool api_RNetServerGetInfo(int cnum, char *param, char *data, int mdrcnt,
 	DEBUG("level %d\n", uLevel);
 
 	/* check it's a supported varient */
-	if (!prefix_ok(str1, "WrLh"))
+	if (!string_has_prefix(str1, "WrLh"))
 		return false;
 	switch (uLevel) {
 	case 0:
@@ -854,7 +848,7 @@ int reply_trans(char *inbuf, char *outbuf, size_t inbuf_len, size_t outbuf_len)
 	DEBUG("trans <%s> data=%d params=%d setup=%d\n", name, tdscnt, tpscnt,
 	      suwcnt);
 
-	if (strncmp(name, "\\PIPE\\", strlen("\\PIPE\\")) == 0) {
+	if (string_has_prefix(name, "\\PIPE\\")) {
 		outsize = named_pipe(cnum, outbuf, name + strlen("\\PIPE\\"),
 		                     setup, data, params, suwcnt, tdscnt,
 		                     tpscnt, msrcnt, mdrcnt, mprcnt);

src/server.c
@@ -182,7 +182,7 @@ static int read_dosattrib(const char *path)
 	}
 	buf[nbytes] = '\0';
 
-	if (strncmp(buf, "0x", 2) != 0) {
+	if (string_has_prefix(buf, "0x")) {
 		/* TODO: Maybe support newer versions */
 		return 0;
 	}
@@ -479,7 +479,7 @@ bool unix_convert(char *name, int cnum, pstring saved_last_component,
 	   directory structure */
 
 	start = name;
-	while (strncmp(start, "./", 2) == 0)
+	while (string_has_prefix(start, "./"))
 		start += 2;
 
 	/* now match each part of the path name separately, trying the names
@@ -586,9 +586,8 @@ int sys_disk_free(char *path, int *bsize, int *dfree, int *dsize)
 
 static bool path_within(const char *path, const char *top)
 {
-	size_t top_len = strlen(top);
-	return strlen(path) >= top_len && strncmp(top, path, top_len) == 0 &&
-	       (path[top_len] == '/' || path[top_len] == '\0');
+	return strcmp(path, top) == 0 ||
+	       (string_has_prefix(path, top) && path[strlen(top)] == '/');
 }
 
 /* Returns true only if the path specified by `name` is contained entirely
@@ -1826,7 +1825,7 @@ int make_connection(char *service, char *dev)
 
 	/* if the request is as a printer and you can't print then refuse */
 	strupper(dev);
-	if (strncmp(dev, "LPT", 3) == 0) {
+	if (string_has_prefix(dev, "LPT")) {
 		WARNING("Attempt to connect to non-printer as a printer\n");
 		return -6;
 	}

src/strfunc.c
@@ -122,6 +122,19 @@ void unix_format(char *fname)
 	}
 }
 
+bool string_has_prefix(const char *s, const char *prefix)
+{
+	return strncmp(s, prefix, strlen(prefix)) == 0;
+}
+
+bool string_has_suffix(const char *s, const char *suffix)
+{
+	size_t s_len = strlen(s);
+	size_t suffix_len = strlen(suffix);
+	return s_len >= suffix_len &&
+	       strcmp(s + s_len - suffix_len, suffix) == 0;
+}
+
 /* Skip past a string in a buffer */
 char *skip_string(char *buf)
 {
@@ -132,7 +145,7 @@ char *skip_string(char *buf)
 bool trim_string(char *s, char *front, char *back)
 {
 	bool ret = false;
-	while (front && *front && strncmp(s, front, strlen(front)) == 0) {
+	while (front && *front && string_has_prefix(s, front)) {
 		char *p = s;
 		ret = true;
 		while (1) {
@@ -141,8 +154,7 @@ bool trim_string(char *s, char *front, char *back)
 			p++;
 		}
 	}
-	while (back && *back && strlen(s) >= strlen(back) &&
-	       strncmp(s + strlen(s) - strlen(back), back, strlen(back)) == 0) {
+	while (back && *back && string_has_suffix(s, back)) {
 		ret = true;
 		s[strlen(s) - strlen(back)] = 0;
 	}
@@ -160,7 +172,7 @@ void unix_clean_name(char *s)
 	string_sub(s, "//", "/");
 
 	/* Remove leading ./ characters */
-	if (strncmp(s, "./", 2) == 0) {
+	if (string_has_prefix(s, "./")) {
 		trim_string(s, "./", NULL);
 		if (*s == 0)
 			pstrcpy(s, "./");
@@ -449,10 +461,8 @@ bool mask_match(char *str, char *regexp, bool trans2)
 				if (rp)
 					*rp = '\0';
 
-				if (cp1[strlen(cp1) - 1] == '*')
-					last_wcard_was_star = true;
-				else
-					last_wcard_was_star = false;
+				last_wcard_was_star =
+				    string_has_suffix(cp1, "*");
 
 				if (!do_match(cp2, cp1)) {
 					break;

src/strfunc.h
@@ -41,6 +41,8 @@ bool mask_match(char *str, char *regexp, bool trans2);
 int put_unicode(char *dst, char *src);
 char *safe_strcpy(char *dest, const char *src, int dest_size);
 char *safe_strcat(char *dest, const char *src, int dest_size);
+bool string_has_prefix(const char *s, const char *prefix);
+bool string_has_suffix(const char *s, const char *suffix);
 
 /* TODO: Remove these once their addition to glibc is less recent */
 #undef strlcat

src/trans2.c
@@ -301,9 +301,7 @@ static int get_lanman2_dir_entry(int cnum, char *path_mask, int dirtype,
 	                 strequal(Connections[cnum].dirpath, "/");
 	bool was_8_3;
 	int nt_extmode; /* Used for NT connections instead of mode */
-	bool needslash =
-	    (Connections[cnum].dirpath[strlen(Connections[cnum].dirpath) - 1] !=
-	     '/');
+	bool needslash = string_has_suffix(Connections[cnum].dirpath, "/");
 
 	*fname = 0;
 	*out_of_space = false;

src/util.c
@@ -173,7 +173,7 @@ int log_output(const char *funcname, int linenum, int level, char *format_str,
 	fflush(log_file);
 
 	n = strlen(format_str);
-	if (n > 0 && format_str[strlen(format_str) - 1] == '\n') {
+	if (n > 0 && string_has_suffix(format_str, "\n")) {
 		log_start_of_line = true;
 	}
 

commit 58b451a3a4e4b8dbb9d394a314dbdcccd30ed22d

Fix another backwards check
Another mistake that I introduced in commit 570571de35a1af5aeed.

Fixes #3, but for WfW as well.

src/dir.c
@@ -393,7 +393,7 @@ bool get_dir_entry(int cnum, char *mask, int dirtype, char *fname, int *size,
 	             strequal(Connections[cnum].dirpath, ".") ||
 	             strequal(Connections[cnum].dirpath, "/"));
 
-	needslash = string_has_suffix(Connections[cnum].dirpath, "/");
+	needslash = !string_has_suffix(Connections[cnum].dirpath, "/");
 
 	if (!Connections[cnum].dirptr)
 		return false;