5d2eae542b999459697ab2b63c037cc8aa6c38b2
Simon Howard <fraggle@soulsphere.org>
2026-04-29 15:07:27 -0400

    Simplify buffer allocation code
    Since `checked_realloc()` never returns `NULL`, we can simplify this
    drastically. Let's just assume we'll always be able to allocate at least
    16KiB of memory.

--- src/reply.c
+++ src/reply.c
@@ -1234,25 +1234,14 @@ static int transfer_file(int infd, int outfd, int n, char *header, int headlen,
                          int align)
 {
 	static char *buf = NULL;
-	static int size = 0;
+	static const int buf_size = 16 * 1024;
 	char *buf1, *abuf;
 	int total = 0;
 
 	DEBUG("n=%d (head=%d)\n", n, headlen);
 
-	if (size == 0) {
-		size = 16 * 1024;
-	}
-
-	while (!buf && size > 0) {
-		buf = (char *) checked_realloc(buf, size + 8);
-		if (!buf)
-			size /= 2;
-	}
-
-	if (!buf) {
-		ERROR("Can't allocate transfer buffer!\n");
-		exit(1);
+	if (buf == NULL) {
+		buf = checked_realloc(buf, buf_size + 8);
 	}
 
 	abuf = buf + (align % 8);
@@ -1261,7 +1250,7 @@ static int transfer_file(int infd, int outfd, int n, char *header, int headlen,
 		n += headlen;
 
 	while (n > 0) {
-		int s = MIN(n, size);
+		int s = MIN(n, buf_size);
 		int ret, ret2 = 0;
 
 		ret = 0;
@@ -1277,7 +1266,7 @@ static int transfer_file(int infd, int outfd, int n, char *header, int headlen,
 		}
 
 		if (header && headlen > 0) {
-			ret = MIN(headlen, size);
+			ret = MIN(headlen, buf_size);
 			memcpy(buf1, header, ret);
 			headlen -= ret;
 			header += ret;
