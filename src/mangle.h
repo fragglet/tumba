c9c1ee9ca902cd6982ee2745ca05f5186f80fd51
Simon Howard <fraggle@soulsphere.org>
2026-04-29 16:23:49 -0400

    Modernize: constify string variables

--- src/mangle.h
+++ src/mangle.h
@@ -13,8 +13,8 @@
 
 struct share;
 
-unsigned int str_checksum(char *s);
-bool is_8_3(char *fname, bool check_case);
-bool is_mangled(char *s);
+unsigned int str_checksum(const char *s);
+bool is_8_3(const char *fname, bool check_case);
+bool is_mangled(const char *s);
 void mangle_name_83(char *s, int s_len);
 void name_map_mangle(char *out_name, bool need83, const struct share *share);
